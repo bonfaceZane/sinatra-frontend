568fe98016fb864162e3b6f5224ddf9177385165
zaneObwoni <zaneobwoni@gmail.com>
2023-12-14 21:14:16 +0100

    updated homepage

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ReactElement } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,7 @@ export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
-}): JSX.Element {
+}): ReactElement {
     return (
         <html lang='en'>
             <body className={inter.className}>{children}</body>
