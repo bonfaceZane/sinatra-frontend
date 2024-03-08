f41cace83225d66af6566b79a75aee1be97b46f4
zaneObwoni <zaneobwoni@gmail.com>
2024-03-08 15:59:07 +0100

    chore: next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,17 @@
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   transpilePackages: ["ui"],
   logging: {
     fetches: {
       fullUrl: true,
     },
   },
+    pageExtensions: ['ts', 'tsx', 'mdx'],
+    experimental: {
+      mdxRs: true,
+    },
 };
+
+
+module.exports = nextConfig;
