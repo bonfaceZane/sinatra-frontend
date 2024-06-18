acd70496274f89dfc3b25b56055f0bdf43acd1de
zaneObwoni <zaneobwoni@gmail.com>
2024-06-18 12:23:32 +0200

    biojme: formater

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,11 @@
-import { ReactElement } from 'react';
-import { Button } from '../components/shared/button';
+import { ReactElement } from "react"
+import { Button } from "../components/shared/button"
 
 export default function Page(): ReactElement {
-    return (
-        <main className="">
-            <h2 className={'text-red-500'}>Sinatra: music to my earsss</h2>
-            <Button>Another button</Button>
-        </main>
-    );
+	return (
+		<main className=''>
+			<h2 className={"text-red-500"}>Sinatra: music to my earsss</h2>
+			<Button>Another button in the stacks</Button>
+		</main>
+	)
 }
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,18 +1,19 @@
+const MillionLint = require("@million/lint")
 // next.config.{js|ts}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['ui', 'lib', '@repo/typescript-config'],
-    logging: {
-        fetches: {
-            fullUrl: true,
-        },
-    },
-    pageExtensions: ['ts', 'tsx', 'mdx'],
-    experimental: {
-        mdxRs: true,
-        reactCompiler: true,
-    },
-};
+	transpilePackages: ["ui", "lib", "@repo/typescript-config"],
+	logging: {
+		fetches: {
+			fullUrl: true,
+		},
+	},
+	pageExtensions: ["ts", "tsx", "mdx"],
+	experimental: {
+		mdxRs: true,
+		reactCompiler: true,
+	},
+}
 
-module.exports = nextConfig;
+module.exports = MillionLint.next({ rsc: true })(nextConfig)
