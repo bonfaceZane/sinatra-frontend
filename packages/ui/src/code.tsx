6a2953a3c6641181f4bffb4110399797facb3993
zaneObwoni <zaneobwoni@gmail.com>
2024-07-02 11:31:05 +0200

    chore: fixed formart for semicolons as needed

--- packages/ui/src/code.tsx
+++ packages/ui/src/code.tsx
@@ -1,9 +1,11 @@
+import type React from "react"
+
 export function Code({
 	children,
 	className,
 }: {
 	children: React.ReactNode
 	className?: string
-}): JSX.Element {
+}): React.ReactElement {
 	return <code className={className}>{children}</code>
 }
